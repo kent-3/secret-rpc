40f3dcc38e84198e6181eecb32041c8417f35c90
kent-3 <100624004+kent-3@users.noreply.github.com>
2024-04-20 00:17:25 -0400

    return Query...Response types

--- src/query/auth.rs
+++ src/query/auth.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 // TODO - Add query methods (not sure if they exist... they are not in secretjs):
 // ModuleAccounts, AccountAddressByID, AddressStringToBytes, AddressBytesToString, Bech32Prefix
 
@@ -17,44 +19,49 @@ pub enum Account {
 }
 
 impl crate::Client {
-    pub async fn auth_account(&self, address: impl Into<String>) -> Result<Account> {
+    // TODO - decide on returning Account vs QueryAccountResponse
+    pub async fn auth_account(&self, address: impl Into<String>) -> Result<QueryAccountResponse> {
         let address = address.into();
 
         let path = "/cosmos.auth.v1beta1.Query/Account";
         let msg = QueryAccountRequest { address };
 
-        let any = self
-            .query_with_msg(path, msg)
+        self.query_with_msg(path, msg)
             .await
             .and_then(try_decode_response::<QueryAccountResponse>)
-            .and_then(|res| {
-                res.account
-                    .ok_or(Error::AbciQuery("no account".to_string()))
-            })?;
 
-        match any.type_url.as_str() {
-            "/cosmos.auth.v1beta1.BaseAccount" => {
-                let account: BaseAccount = any.to_msg()?;
-                Ok(Account::BaseAccount(account.try_into()?))
-            }
-            "/cosmos.auth.v1beta1.ModuleAccount" => {
-                let account: ModuleAccount = any.to_msg()?;
-                Ok(Account::ModuleAccount(account.try_into()?))
-            }
-            // trait 'Name' is not implemented for these
-            "/cosmos.vesting.v1beta1.ContinuousVestingAccount" => {
-                let account = try_decode_any::<ContinuousVestingAccount>(any)?;
-                Ok(Account::ContinuousVestingAccount(account.try_into()?))
-            }
-            "/cosmos.vesting.v1beta1.DelayedVestingAccount" => {
-                let account = try_decode_any::<DelayedVestingAccount>(any)?;
-                Ok(Account::DelayedVestingAccount(account.try_into()?))
-            }
-            _ => Err(Error::AbciQuery(format!(
-                "unexpected type_url: {}",
-                any.type_url
-            ))),
-        }
+        // let any = self
+        //     .query_with_msg(path, msg)
+        //     .await
+        //     .and_then(try_decode_response::<QueryAccountResponse>);
+        //     .and_then(|res| {
+        //         res.account
+        //             .ok_or(Error::AbciQuery("no account".to_string()))
+        //     })?;
+        //
+        // match any.type_url.as_str() {
+        //     "/cosmos.auth.v1beta1.BaseAccount" => {
+        //         let account: BaseAccount = any.to_msg()?;
+        //         Ok(Account::BaseAccount(account.try_into()?))
+        //     }
+        //     "/cosmos.auth.v1beta1.ModuleAccount" => {
+        //         let account: ModuleAccount = any.to_msg()?;
+        //         Ok(Account::ModuleAccount(account.try_into()?))
+        //     }
+        //     // trait 'Name' is not implemented for these
+        //     "/cosmos.vesting.v1beta1.ContinuousVestingAccount" => {
+        //         let account = try_decode_any::<ContinuousVestingAccount>(any)?;
+        //         Ok(Account::ContinuousVestingAccount(account.try_into()?))
+        //     }
+        //     "/cosmos.vesting.v1beta1.DelayedVestingAccount" => {
+        //         let account = try_decode_any::<DelayedVestingAccount>(any)?;
+        //         Ok(Account::DelayedVestingAccount(account.try_into()?))
+        //     }
+        //     _ => Err(Error::AbciQuery(format!(
+        //         "unexpected type_url: {}",
+        //         any.type_url
+        //     ))),
+        // }
     }
 
     // TODO - more work needed here to handle the response and pagination...
@@ -70,32 +77,33 @@ impl crate::Client {
             .and_then(try_decode_response::<QueryAccountsResponse>)
     }
 
-    pub async fn auth_params(&self) -> Result<Params> {
+    pub async fn auth_params(&self) -> Result<QueryParamsResponse> {
         let path = "/cosmos.auth.v1beta1.Query/Params";
         let msg = QueryParamsRequest {};
 
         self.query_with_msg(path, msg)
             .await
             .and_then(try_decode_response::<QueryParamsResponse>)
-            .and_then(|x| x.params.ok_or(Error::AbciQuery("empty params".to_string())))
+        // .and_then(|x| x.params.ok_or(Error::AbciQuery("empty params".to_string())))
     }
 
     pub async fn auth_module_account_by_name(
         &self,
         name: impl Into<String>,
-    ) -> Result<ModuleAccount> {
+    ) -> Result<QueryModuleAccountByNameResponse> {
         let name = name.into();
 
         let path = "/cosmos.auth.v1beta1.Query/ModuleAccountByName";
-        let msg = QueryModuleAccountByNameRequest { name: name.clone() };
+        let msg = QueryModuleAccountByNameRequest { name };
 
         self.query_with_msg(path, msg)
             .await
             .and_then(try_decode_response::<QueryModuleAccountByNameResponse>)
-            .and_then(|res| {
-                res.account
-                    .ok_or(Error::AbciQuery("empty account".to_string()))
-            })
-            .and_then(try_decode_any::<ModuleAccount>)
+        // .and_then(|res| {
+        //     res.account
+        //         .ok_or(Error::AbciQuery("empty account".to_string()))
+        // })
+        // .and_then(|any| any.to_msg::<ModuleAccount>().map_err(Error::from))
+        // .and_then(|acc| acc.try_into().map_err(Error::from))
     }
 }
